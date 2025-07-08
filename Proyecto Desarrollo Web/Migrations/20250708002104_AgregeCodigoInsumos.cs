1af5afda4ebff22ec608e38c6eeb90f7dda4daf0
Santiago Vieitez <savieitez@gmail.com>
2025-07-07 21:24:06 -0300

    CambiosConInsumos

---
+++ Proyecto Desarrollo Web/Migrations/20250708002104_AgregeCodigoInsumos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_Desarrollo_Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregeCodigoInsumos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Codigo",
+                table: "Insumos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Codigo",
+                table: "Insumos");
+        }
+    }
+}
--- Proyecto Desarrollo Web/Models/InsumoInformatico.cs.cs
+++ Proyecto Desarrollo Web/Models/InsumoInformatico.cs.cs
@@ -3,6 +3,7 @@
     public class InsumoInformatico
     {
         public int Id { get; set; }
+        public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public string Marca { get; set; }
