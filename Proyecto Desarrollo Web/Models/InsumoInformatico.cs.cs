67122a2918ac4c6a007ff102630cd76409c7582b
Santiago Vieitez <savieitez@gmail.com>
2025-07-07 21:24:32 -0300

    MiniCambioInsumo

--- Proyecto Desarrollo Web/Models/InsumoInformatico.cs.cs
+++ Proyecto Desarrollo Web/Models/InsumoInformatico.cs.cs
@@ -3,11 +3,11 @@
     public class InsumoInformatico
     {
         public int Id { get; set; }
-        public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public string Marca { get; set; }
         public int Stock { get; set; }
         public decimal Precio { get; set; }
+        public string Codigo { get; set; }
     }
 }
